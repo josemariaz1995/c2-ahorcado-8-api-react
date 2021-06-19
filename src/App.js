e304e987473685b983e66f958df7700357d03b6d
Jose Maria Zamora Perez <jzamora@biada.es>
2021-06-19 16:57:49 +0200

    llamada a la api recibiendo una palabra

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-
+import { useCallback, useEffect, useState } from "react";
 import { Ahorcado } from "./Components/Ahorcado";
 import { Mensaje } from "./Components/Mensaje";
 
 function App() {
+  const urlApi = "http://localhost:3001/palabras";
+  const [palabraSecreta, setPalabraSecreta] = useState({});
+  const [palabra, setPalabra] = useState("");
+  /* const nFallos = 0;
+  const maxFallos = 11; */
+
+  const getPalabra = useCallback(async (url) => {
+    const response = await fetch(url);
+    const { lista } = await response.json();
+
+    setPalabra(getPalabraAleatoria(lista));
+  }, []);
+  const getPalabraAleatoria = (palabras) => {
+    const posicionAleatoria = Math.floor(Math.random() * palabras.length);
+    return palabras[posicionAleatoria];
+  };
+  useEffect(() => getPalabra(urlApi), [getPalabra]);
+  console.log(palabra);
   return (
     <>
       <Ahorcado />
       <ul className="palabra"></ul>
-      <input type="text" className="letra" maxlength="1" />
+      <input type="text" className="constra" maxlength="1" />
       <ul className="letras-usadas"></ul>
       <Mensaje />
     </>
