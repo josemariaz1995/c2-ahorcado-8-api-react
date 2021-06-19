6455b19cfa2db436a5f29f77b771206016e0bc3f
Albert Araque <aaraquedesarrollo@gmail.com>
2021-06-20 00:26:37 +0200

    Añadidas funciones y comentarios

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import { Mensaje } from "./Components/Mensaje";
 import { Palabra } from "./Components/Palabra";
 
 function App() {
-  const urlApi = "http://localhost:3001/palabras";
+  const urlsApis = {
+    apiPalabras: "http://localhost:3001/palabras",
+    apiAhorcado: "https://letras-ahorcado.herokuapp.com/letras/",
+  };
   const [palabra, setPalabra] = useState([]);
   const [palabraSecreta, setPalabraSecreta] = useState("");
   const [nFallos, setFallos] = useState(0);
@@ -22,7 +25,10 @@ function App() {
     return palabras[posicionAleatoria];
   };
 
-  useEffect(() => getPalabra(urlApi), [getPalabra]);
+  useEffect(
+    () => getPalabra(urlsApis.apiPalabras),
+    [urlsApis.apiPalabras, getPalabra]
+  );
 
   const anyadirLetras = (e) => {
     setPalabra([...palabra, e.target.value]);
@@ -30,6 +36,12 @@ function App() {
       e.target.value = "";
     }, 500);
   };
+
+  const comprobarLetra = (letra) => {};
+
+  const acierto = (arrayPosiciones) => {};
+
+  const error = (mensaje) => {};
 
   return (
     <>
@@ -43,7 +55,11 @@ function App() {
         onChange={anyadirLetras}
         maxLength="1"
       />
+      {/* Al componenete letras usadas le tendremos que pasar un array de letras que el jugador haya usado*/}
       <ul className="letras-usadas"></ul>
+      {/* Al componenente Mensaje le tendremos que pasar una variable booleana 
+      que nos indique si el jugador ha perdido o ganado y mostrar el mensaje
+      de ganar o perder*/}
       <Mensaje />
     </>
   );
